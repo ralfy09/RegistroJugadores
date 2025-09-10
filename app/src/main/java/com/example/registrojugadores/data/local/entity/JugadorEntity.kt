0fd202ea3f1d9b9d4123d716ad27a000b87d6470
ralfy <ralfy@RALFY>
2025-09-10 13:46:55 -0300

    El JugadorEntity.kt

--- app/src/main/java/com/example/registrojugadores/data/local/entity/JugadorEntity.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/entity/JugadorEntity.kt
@@ -2,6 +2,7 @@ package com.example.registrojugadores.data.local.entity
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+
 @Entity(tableName = "Jugadores")
 data class JugadorEntity(
     @PrimaryKey(autoGenerate = true)
