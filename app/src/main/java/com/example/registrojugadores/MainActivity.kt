f1384b959a5d67fd9724878cd80b9f1bbd8e504b
ralfy <ralfy@RALFY>
2025-09-17 14:45:22 -0300

    MainActivity.kt

--- app/src/main/java/com/example/registrojugadores/MainActivity.kt
+++ app/src/main/java/com/example/registrojugadores/MainActivity.kt
@@ -1,38 +1,36 @@
 package com.example.registrojugadores
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
-import com.example.registrojugadores.data.local.database.JugadorDb
 import com.example.registrojugadores.data.repository.JugadorRepository
 import com.example.registrojugadores.presentation.jugador.JugadorViewModel
 import com.example.registrojugadores.presentation.navigation.JugadoresNavHost
+import com.example.registrojugadores.presentation.partida.PartidaViewModel
 import com.example.registrojugadores.ui.theme.RegistroJugadoresTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val database = Room.databaseBuilder(
-            applicationContext,
-            JugadorDb::class.java,
-            "Jugador.db"
-        ).fallbackToDestructiveMigration()
-            .build()
-
-        val jugadorRepository = JugadorRepository(database.jugadorDao())
-        val jugadorViewModel = JugadorViewModel(jugadorRepository)
-
         setContent {
             RegistroJugadoresTheme {
                 val navController = rememberNavController()
+                val jugadorViewModel: JugadorViewModel = hiltViewModel()
+                val partidaViewModel: PartidaViewModel = hiltViewModel()
+
                 JugadoresNavHost(
                     navHostController = navController,
-                    jugadorViewModel = jugadorViewModel
+                    jugadorViewModel = jugadorViewModel,
+                    partidaViewModel = partidaViewModel
                 )
             }
         }
