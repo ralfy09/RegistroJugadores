79a0c143523e3795cb6cd3d5e5c6d3302af9d24b
ralfy <ralfy@RALFY>
2025-09-17 14:45:51 -0300

    JugadorDb.kt

--- app/src/main/java/com/example/registrojugadores/data/local/database/JugadorDb.kt
+++ app/src/main/java/com/example/registrojugadores/data/local/database/JugadorDb.kt
@@ -2,16 +2,21 @@ package com.example.registrojugadores.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.example.registrojugadores.data.local.dao.JugadorDao
+import com.example.registrojugadores.data.local.dao.PartidaDao
 import com.example.registrojugadores.data.local.entity.JugadorEntity
+import com.example.registrojugadores.data.local.entity.PartidaEntity
 
 
 @Database(
-    entities = [JugadorEntity::class],
-    version = 1,
+    entities = [JugadorEntity::class, PartidaEntity::class],
+    version = 2,
     exportSchema = false
 )
-
-abstract class JugadorDb : RoomDatabase() {
+@TypeConverters(RoomConverters::class)
+abstract class AppDatabase: RoomDatabase() {
     abstract fun jugadorDao(): JugadorDao
+
+    abstract fun PartidaDao (): PartidaDao
 }
