3d25e516523ba2cf7039c108151f644b68361ce1
ralfy <ralfy@RALFY>
2025-09-17 14:40:45 -0300

    RoomConverters.kt

---
+++ app/src/main/java/com/example/registrojugadores/data/local/database/RoomConverters.kt
@@ -0,0 +1,16 @@
+package com.example.registrojugadores.data.local.database
+
+import androidx.room.TypeConverter
+import java.util.Date
+
+class RoomConverters {
+    @TypeConverter
+    fun fromTimestamp(value: Long?): Date? {
+        return value?.let { Date(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date?): Long? {
+        return date?.time
+    }
+}
