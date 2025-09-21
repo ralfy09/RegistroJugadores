4d9abf6781ef198364d17efe2cf805cea0f428af
ralfy <ralfy@RALFY>
2025-09-21 11:02:25 -0300

    le agregue el Logros

--- app/src/main/java/com/example/registrojugadores/MainActivity.kt
+++ app/src/main/java/com/example/registrojugadores/MainActivity.kt
@@ -6,9 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
-import androidx.room.Room
-import com.example.registrojugadores.data.repository.JugadorRepository
 import com.example.registrojugadores.presentation.jugador.JugadorViewModel
+import com.example.registrojugadores.presentation.logro.LogroViewModel
 import com.example.registrojugadores.presentation.navigation.JugadoresNavHost
 import com.example.registrojugadores.presentation.partida.PartidaViewModel
 import com.example.registrojugadores.ui.theme.RegistroJugadoresTheme
@@ -26,11 +25,13 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
                 val jugadorViewModel: JugadorViewModel = hiltViewModel()
                 val partidaViewModel: PartidaViewModel = hiltViewModel()
+                val logroViewModel: LogroViewModel = hiltViewModel()
 
                 JugadoresNavHost(
                     navHostController = navController,
                     jugadorViewModel = jugadorViewModel,
-                    partidaViewModel = partidaViewModel
+                    partidaViewModel = partidaViewModel,
+                    logroViewModel = logroViewModel
                 )
             }
         }
